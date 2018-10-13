2e815f333d8f2a026b4102144d9d6d89ab0d6710
mrright <275068651@qq.com>
2018-10-13 22:44:32 +0800

    TestParseCityList

--- crawler/fetcher/fetcher.go
+++ crawler/fetcher/fetcher.go
@@ -7,7 +7,6 @@ import (
 	"golang.org/x/text/encoding"
 	"golang.org/x/text/encoding/unicode"
 	"golang.org/x/text/transform"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -25,15 +24,16 @@ func Fetch(url string) ([]byte, error) {
 			fmt.Errorf("wrong status code: %d", resp.StatusCode)
 	}
 
-	e := determineEncoding(resp.Body)
-	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
+	bodyRead := bufio.NewReader(resp.Body)
+	e := determineEncoding(bodyRead)
+	utf8Reader := transform.NewReader(bodyRead, e.NewDecoder())
 	all, err := ioutil.ReadAll(utf8Reader)
 
 	return all, err
 }
 
-func determineEncoding(r io.ReadCloser) encoding.Encoding {
-	bytes, err := bufio.NewReader(r).Peek(1024)
+func determineEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
 	if err != nil {
 		log.Printf("Fetcher error: %v", err)
 		return unicode.UTF8
--- crawler/zhenai/parser/citylist_test.go
+++ crawler/zhenai/parser/citylist_test.go
@@ -1,7 +1,31 @@
 package parser
 
-import "testing"
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+)
 
 func TestParseCityList(t *testing.T) {
+	contens, err := ioutil.ReadFile("citylist_test_data.html")
 
+	if err != nil {
+		panic(err)
+	}
+
+	result := ParseCityList(contens)
+
+	const resultSize = 470
+
+	fmt.Println(len(result.Request))
+
+	if len(result.Request) != resultSize {
+		t.Errorf("result should have %d" +
+			"requests;but had %d", resultSize, len(result.Request))
+	}
+
+	if len(result.Items) != resultSize {
+		t.Errorf("result should have %d" +
+			"requests;but had %d", resultSize, len(result.Items))
+	}
 }
