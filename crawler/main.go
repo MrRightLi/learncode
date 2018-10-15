db76673891d7999838d634dee0c40a8c3367bf00
MrRightli <>
2018-10-15 18:54:42 +0800

    数据存储架构

--- crawler/main.go
+++ crawler/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"project/learncode/crawler/engine"
+	"project/learncode/crawler/persist"
 	"project/learncode/crawler/scheduler"
 	"project/learncode/crawler/zhenai/parser"
 )
@@ -10,6 +11,7 @@ func main() {
 	e := engine.ConcurrentEngine{
 		Scheduler:   &scheduler.SimpleScheduler{},
 		WorkerCount: 100,
+		ItemChan: persist.ItemSaver(),
 	}
 	e.Run(engine.Request{
 		Url:        "http://www.zhenai.com/zhenghun",
