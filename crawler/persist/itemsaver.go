cab746a4bd2fd4c8e8dd89d523e9a3b3df5771ef
MrRightli <>
2018-10-16 15:06:20 +0800

    elasticsearch 集成测试

--- crawler/persist/itemsaver.go
+++ crawler/persist/itemsaver.go
@@ -1,6 +1,10 @@
 package persist
 
-import "log"
+import (
+	"context"
+	"github.com/olivere/elastic"
+	"log"
+)
 
 func ItemSaver() chan interface{} {
 	out := make(chan interface{})
@@ -10,7 +14,28 @@ func ItemSaver() chan interface{} {
 			item := <- out
 			log.Printf("Item Saver Got item: #%d: %v", itemCount, item)
 			itemCount++
+
+			save(item)
 		}
 	}()
 	return out
+}
+
+func save(item interface{}) (id string, err error) {
+	client, err := elastic.NewClient(
+		// Must turn off sniffer
+		elastic.SetSniff(false))
+
+	if err != nil {
+		return "", err
+	}
+	response, err := client.Index().
+		Index("dataing_profile").
+		Type("zhengai").
+		BodyJson(item).
+		Do(context.Background())
+	if err != nil {
+		return "", err
+	}
+	return response.Id, nil
 }
---
+++ crawler/persist/itemsaver_test.go
@@ -0,0 +1,65 @@
+package persist
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/olivere/elastic"
+	"project/learncode/crawler/model"
+	"testing"
+)
+
+func TestSave(t *testing.T) {
+
+	excepted := model.Profile{
+		Age:34,
+		Height:162,
+		Weight:"57",
+		Income:"3001-500元",
+		Gender:"女",
+		Name:"安静的雪",
+		Xinzuo:"牡羊座",
+		Occupation:"人事",
+		Marriage:"离异",
+		House:"已购房",
+		Hokou:"山东菏泽",
+		Education:"大学本科",
+		Car:"未购车",
+	}
+
+	id, err := save(excepted)
+
+	if err != nil {
+		panic(err)
+	}
+
+	client, err := elastic.NewClient(elastic.SetSniff(false))
+
+	if err != nil {
+		panic(err)
+	}
+
+	response, err := client.Get().
+		Index("dataing_profile").
+		Type("zhengai").
+		Id(id).Do(context.Background())
+
+	if err != nil {
+		panic(err)
+	}
+
+	t.Logf("%s", response.Source)
+
+	var actual model.Profile
+	err = json.Unmarshal([]byte(*response.Source), &actual)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if actual != excepted {
+		t.Errorf("got %v; expected: %v", actual, excepted)
+	} else {
+		fmt.Printf("got actual: %+v", actual)
+	}
+}
